7f47048b75e7b6c3a0895405ea594f2e5057a72b
csfahad <dev.csfahad@gmail.com>
2026-08-12 20:39:38 +0530

    feat: add prod vercel-analytics

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { AppProviders } from "./providers/AppProviders.tsx";
-
+import { Analytics } from '@vercel/analytics/react';
+ 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <AppProviders>
             <App />
+	    <Analytics />
         </AppProviders>
     </StrictMode>,
 );
